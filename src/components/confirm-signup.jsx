fe3e6c2219443868568c28053c634af63ca232bf
brian890809 <brian08090809@gmail.com>
2025-03-18 10:58:36 -0700

    cognito-signup-WIC

---
+++ src/components/confirm-signup.jsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import { handleConfirmSignUp } from "@/lib/cognitoActions";
+import { useFormStatus } from "react-dom";
+import { useActionState } from "react";
+
+function ConfirmButton({children}) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" className="w-full" aria-disabled={pending}>
+      {children}
+    </Button>
+  );
+}
+
+export function ConfirmPage({
+  className,
+  ...props
+}) {
+  const [errorMessage, dispatch] = useActionState(handleConfirmSignUp, undefined);
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Please confirm your email</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form action={dispatch}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" required />
+              </div>
+              <div className="grid gap-3">
+                <Label >Code</Label>
+                <Input id="code" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <ConfirmButton>
+                  Confirm
+                </ConfirmButton>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="/signup" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Didn&apos;t receive an email?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Resend Code
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ src/components/login-form.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import { useFormStatus } from "react-dom";
+import { useActionState } from "react";
+import { handleSignIn } from "@/lib/cognitoActions";
+
+function LoginButton({children}) {
+  const { pending } = useFormStatus();
+  return (
+    <Button type="submit" className="w-full" aria-disabled={pending}>
+        {children}
+    </Button>
+  );
+}
+
+export function LoginForm({
+  className,
+  ...props
+}) {
+  const [errorMessage, dispatch] = useActionState(handleSignIn, undefined);
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={dispatch}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" required />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <LoginButton>
+                  Login
+                </LoginButton>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="/signup" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
